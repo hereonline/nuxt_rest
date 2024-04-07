d8d47fe513a7436bf53e06c2510e316b27555edc
Klabautterr <htmlsite@skiff.com>
2024-04-07 18:21:06 +0200

    loeschen geht

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
-// Ensure it uses `ref` instead of `reactive`.
+
 import UploadForm from './UploadForm.vue'; 
+
 const sort = ref({
   column: 'name',
-  direction: 'desc'
+  direction: 'desc',
 })
 
 const columns = [
+{
+  label: 'Id',
+  key: 'id',
+
+},
 {
   label: 'Name',
   key: 'name',
@@ -22,18 +28,30 @@ const columns = [
 
 },
 {
-  label: 'Delete',
-  key: 'delete',
+  key: 'actions'
+}]
 
-},
-]
 
 const { data: count } = await useFetch('http://localhost:8081/event')
+
+const deleteId = ref('');
+
+const deleteRow = async (id: number) => {
+  await fetch(`http://localhost:8081/event/${id}`, {
+      method: 'DELETE',
+        });
+
+    }
+
 
 </script>
 
 <template>
-  <UTable :columns="columns" :rows="count" />
-      <UButton row="delete" >Löschen</UButton>
+    <UTable :columns="columns" :rows="count" >
+      <template #actions-data="{ row }">   
+        <UButton @click="deleteRow(row.id)" color="green" variant="ghost" icon="i-heroicons-trash-20-solid" />
+      </template>
+    </UTable>
   <UploadForm/>
 </template>
+
